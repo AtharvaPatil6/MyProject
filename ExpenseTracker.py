f404306d0fed72f4a4d31283ca029895808cb530
Atharva Patil <atharvapatil0605@gmail.com>
2026-06-06 13:50:14 +0530

    Add files via upload
    A simple Python-based Expense Tracker that allows users to add, view, delete, and manage expenses while tracking total spending. Data can be saved to a file using Python file handling.

---
+++ ExpenseTracker.py
@@ -0,0 +1,62 @@
+expense=[]
+total=0
+
+while True:
+    print("--- Welcome to Expense Tracker ---")
+    print("1.Add Expense")
+    print("2.View All Expense")
+    print("3.View Total Spending")
+    print("4.To delete a Expense")
+    print("5.To Create Expense data file")
+    print("6.Exit")
+    choice=int(input("Enter Your Choice(1/2/3/4/5/6): "))
+
+    if choice==1:
+        date=input("Enter Date: ") 
+        category=input("Enter Expense Category: ") 
+        descp=input("More Description: ") 
+        amount=float(input("Enter The Amount: "))
+
+        expenses={
+            "date":date,
+            "category":category,
+            "description":descp,
+            "amount":amount
+        }     
+
+        expense.append(expenses)
+    
+    elif choice==2:
+        count=0
+        for exp in expense:
+            print(f"Expense: {count} :- Date: {exp["date"]}, Category: {exp["category"]}, Description:{exp["description"]}, amount:{exp["amount"]}")
+            count+=1
+    
+   
+    elif choice == 3:
+        total = 0
+
+        for i in expense:
+            total += i["amount"]
+
+        print("Total Spending =", total)
+
+    elif choice==4:
+        delete=input("Enter Expense you want to delete?: ").lower()
+        for exp in expense:
+            if exp["category"].lower() == delete:
+                expense.remove(exp)
+                found=True
+                print("Expense Removed Succesfully!!")
+            
+        if not found:
+            print("Expense Not found!")
+        
+    elif choice==5:
+        with open("Expensedata.txt","w") as f:
+            data=f.write(str(expense))
+            print("Data file Created Successfully!!!")
+    
+    elif choice==6:
+        print("Thanks for using the app!!")
+        break
